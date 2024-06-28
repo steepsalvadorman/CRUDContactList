0b04bca92ab66cd4f8271a6f0b31c6b1b4d0632d
Steep <stevesalvadorman@gmail.com>
2024-06-28 06:43:31 -0500

    contactlist terminado

---
+++ src/app/contact-form/contact-form.component.ts
@@ -0,0 +1,75 @@
+import { Component, inject, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { ContactService } from '../services/contact.service';
+import { Contact } from '../model/contact.interface';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-contact-form',
+  standalone: true,
+  imports: [RouterModule, ReactiveFormsModule],
+  templateUrl: './contact-form.component.html',
+  styleUrl: './contact-form.component.css',
+})
+export default class ContactFormComponent implements OnInit {
+  private fb = inject(FormBuilder);
+  private contactService = inject(ContactService);
+  private router = inject(Router);
+  private route = inject(ActivatedRoute);
+
+  form?: FormGroup;
+  contact?: Contact;
+  errors: string[] = [];
+
+  public ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    if (id) {
+      this.contactService.get(parseInt(id)).subscribe((contact) => {
+        this.contact = contact;
+        this.form = this.fb.group({
+          name: [contact.name, [Validators.required]],
+          email: [contact.email, [Validators.required, Validators.email]],
+        });
+      });
+    } else {
+      this.form = this.fb.group({
+        name: ['', [Validators.required]],
+        email: ['', [Validators.required, Validators.email]],
+      });
+    }
+  }
+
+  save() {
+    if (this.form?.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+
+    const contactForm = this.form!.value;
+    let request: Observable<Contact>;
+
+    if (this.contact) {
+      request = this.contactService.update(this.contact.id, contactForm)
+
+    } else {
+      this.contactService.create(contactForm)
+        request = this.contactService.create(contactForm);
+    }
+
+    request.subscribe({
+      next: () => {
+        this.errors = [];
+        this.router.navigate(['/']);
+      },
+      error: (response) => {
+        this.errors = response.error.errors;
+      },
+    });
+  }
+}
---
+++ src/app/contact-list/contact-list.component.ts
@@ -0,0 +1,40 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { ContactService } from '../services/contact.service';
+import { DatePipe } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { Contact } from '../model/contact.interface';
+
+@Component({
+  selector: 'app-contact-list',
+  standalone: true,
+  imports: [DatePipe, RouterModule],
+  templateUrl: './contact-list.component.html',
+  styleUrl: './contact-list.component.css'
+})
+export default class ContactListComponent implements OnInit{
+  private contactService = inject(ContactService);
+
+  contacts: Contact[] = [];
+
+
+  ngOnInit(): void {
+    this.loadAll();
+  };
+
+  loadAll() {
+    this.contactService.list()
+      .subscribe((contacts) => {
+      this.contacts = contacts;
+    });
+  }
+
+  deleteContact(contact: Contact) {
+    this.contactService.delete(contact.id)
+      .subscribe(() => {
+        console.log('Contact deleted');
+      });
+    }
+
+  }
+
+
